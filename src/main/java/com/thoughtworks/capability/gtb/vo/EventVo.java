d9028b5ce04efd59827a11c9fd61583ffb9f96d9
ruitao <harrytaorui@gmail.com>
2020-11-05 00:12:29 +0800

    [taorui]task1&2完成

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,18 +1,24 @@
 package com.thoughtworks.capability.gtb.vo;
 
-import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.thoughtworks.capability.gtb.serializer.DateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class EventVo {
 
-  private String id;
-  private String name;
-  private EventType type;
-  private Date time;
-  private UserVo user;
+    private String id;
+    private String name;
+    private EventType type;
+    @JsonSerialize(using = DateSerializer.class)
+    private Date time;
+    @JsonUnwrapped
+    private UserVo user;
 }
